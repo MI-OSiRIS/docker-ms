bac36d19db8f2189270cc6b10152f124e6d2f623
Ezra <ezkissel@indiana.edu>
2019-03-07 14:00:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ sudo /etc/init.d/mongodb start
 sudo /etc/init.d/redis-server start
 
 echo "UNIS IP : `hostname --ip-address`"
-periscoped --port 8889 -d DEBUG -c /etc/periscope/unis.cfg
+periscoped --port 8889 -d DEBUG -c /etc/periscope/ms.cfg
